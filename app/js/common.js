9925c2aba1bac67236e1a40043d8ca4e491f7c3e
Jktrcfyl <Oleksandr23061998@gmail.com>
2016-09-29 02:38:50 +0300

    test

--- app/js/common.js
+++ app/js/common.js
@@ -34,7 +34,15 @@ $(function() {
 	} catch(err) {
 
 	};
-
 	$("img, a").on("dragstart", function(event) { event.preventDefault(); });
+
+	$('.dropdown').on('show.bs.dropdown', function(e){
+    $(this).find('.dropdown-menu').first().stop(true, true).slideDown();
+  });
+
+	  $('.dropdown').on('hide.bs.dropdown', function(e){
+    $(this).find('.dropdown-menu').first().stop(true, true).slideUp();
+  });
+
 
 });
